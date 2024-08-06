2dd692a857bf2931e579ddbd14278995522382f2
Unicornimpersonated <137487626+Unicornimpersonated@users.noreply.github.com>
2024-08-06 23:06:00 +0530

    Add files via upload

---
+++ userlog.js
@@ -0,0 +1,23 @@
+// document.getElementById('signin').addEventListener('submit', function (e) {
+//     e.preventDefault();
+
+//     // Get user input
+//     const email = document.getElementById('email').value;
+//     const password = document.getElementById('password').value;
+
+//     // You can perform client-side validation here if needed
+
+//     // Simulate a registration request (replace this with actual server-side code)
+//     const registrationData = { email,password };
+//     console.log('Sending registration request:', registrationData);
+
+//     // Display a success message (you should handle success/failure appropriately)
+    
+//     setTimeout(function () {
+//         window.location.href = 'http://127.0.0.1:5500/Virtual%20Hearing/index.html'; // Replace 'main.html' with your main page URL
+//     }, 2000); // 2000 milliseconds (2 seconds) delay, adjust as needed
+// });
+// document.getElementById('register').addEventListener('click', function () {
+//     Redirect to the sign-in page
+//     window.location.href = 'userlogin.html'; // Replace 'signin.html' with your sign-in page URL
+// // });
